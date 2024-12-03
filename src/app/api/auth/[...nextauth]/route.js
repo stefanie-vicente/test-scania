5145be2c328efe7a5e2760fcca6fcb11d435c593
Stefanie Vicente <stefanie.vicente@gmail.com>
2024-12-03 02:04:52 +0100

    implement auth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "repo user",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
